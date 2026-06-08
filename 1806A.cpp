7b69cc28f745f9f173c755d6c82cd5e6002c9dd4
thisisrohit01 <thisisrohit01@gmail.com>
2026-06-08 23:56:59 +0530

    still on 800

---
+++ 1806A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(d-b<0)cout<<-1;
+    }
+}
